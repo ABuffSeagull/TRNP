524d51ac960857f1471d1108366948dd8feff9da
ABuffSeagull <reecevanatta@gmail.com>
2020-04-04 18:13:04 -0400

    Initial mix init

---
+++ lib/trnp.ex
@@ -0,0 +1,18 @@
+defmodule Trnp do
+  @moduledoc """
+  Documentation for `Trnp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Trnp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/trnp_test.exs
@@ -0,0 +1,8 @@
+defmodule TrnpTest do
+  use ExUnit.Case
+  doctest Trnp
+
+  test "greets the world" do
+    assert Trnp.hello() == :world
+  end
+end
