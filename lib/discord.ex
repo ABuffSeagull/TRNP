f0f13322cd34a23a7b382bf770c6f130eaab3392
ABuffSeagull <reecevanatta@gmail.com>
2020-04-16 00:35:21 -0400

    Remove a bunch of dead code

--- lib/discord.ex
+++ lib/discord.ex
@@ -6,60 +6,27 @@ defmodule Discord do
 
   use Nostrum.Consumer
 
-  alias Nostrum.Api
-  alias Nostrum.Cache
-  alias Nostrum.Struct.Guild
-  alias Nostrum.Struct.Guild.Member
   alias Nostrum.Struct.Message
 
   require Logger
 
   @bot_id Application.fetch_env!(:trnp, :bot_id)
-  @guild_id Application.fetch_env!(:trnp, :guild_id)
-  @role_message_id Application.fetch_env!(:trnp, :role_message_id)
-  @selling_channel_id Application.fetch_env!(:trnp, :selling_channel_id)
+  @admin_channel_id Application.fetch_env!(:trnp, :admin_channel_id)
 
-  def start_link do
-    Consumer.start_link(__MODULE__, name: Discord)
-  end
+  def start_link, do: Consumer.start_link(__MODULE__, name: Discord)
 
   def handle_event({:READY, _data, _ws_state}), do: Logger.info("Connected!")
-
-  def handle_event(
-        {:MESSAGE_REACTION_ADD,
-         %{user_id: user_id, message_id: @role_message_id, emoji: %{name: emoji}}, _ws_state}
-      ) do
-    %Guild{members: %{^user_id => %Member{roles: roles}}} = Cache.GuildCache.get!(@guild_id)
-
-    role_a = Application.fetch_env!(:trnp, :role_a_id)
-    role_b = Application.fetch_env!(:trnp, :role_b_id)
-
-    roles = [
-      case String.trim(emoji) do
-        "🅰️" -> role_a
-        "🅱️" -> role_b
-      end
-      | roles
-        |> Enum.filter(&(&1 not in [role_a, role_b]))
-    ]
-
-    Api.modify_guild_member!(@guild_id, user_id, roles: roles)
-  end
 
   # Ignore all messages from self
   def handle_event({:MESSAGE_CREATE, %Message{author: %{id: @bot_id}}, _ws_state}), do: nil
 
-  # when author_id != 696_117_361_044_095_126 do
+  # Handle Admin commands
   def handle_event(
-        {:MESSAGE_CREATE,
-         %Message{
-           channel_id: @selling_channel_id
-         } = message, _ws_state}
-      ) do
-    Trnp.Buying.update(message)
-  end
+        {:MESSAGE_CREATE, %Message{channel_id: @admin_channel_id, content: "!" <> command},
+         _ws_state}
+      ),
+      do: Commands.Admin.handle_command(command)
 
-  def handle_event(_event) do
-    # IO.inspect(event, label: "Unhandled event")
-  end
+  # Fallthrough so it doesn't error
+  def handle_event(_event), do: nil
 end
