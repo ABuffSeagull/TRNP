a6f7768a89d3c84d55ef495757aecfe82d5849dc
ABuffSeagull <reecevanatta@gmail.com>
2020-04-16 00:25:44 -0400

    Some config options
    - Reduce log level
    - Add a time zone database
    - Add channel clean to scheduler

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,17 @@
 import Config
 
+config :logger,
+  level: :info
+
+config :elixir, :time_zone_database, Tzdata.TimeZoneDatabase
+
+config :trnp, Scheduler.Quantum,
+  jobs: [
+    channel_remake: [
+      schedule: "0 0 * * *",
+      task: {Trnp.Selling, :clean_channel, []},
+      timezone: "America/Los_Angeles"
+    ]
+  ]
+
 import_config "#{Mix.env()}.secret.exs"
