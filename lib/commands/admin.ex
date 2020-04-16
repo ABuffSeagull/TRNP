27fdd6626781830d58c54b92e29624b938506fcd
ABuffSeagull <reecevanatta@gmail.com>
2020-04-16 00:36:48 -0400

    Add an Admin Command module

---
+++ lib/commands/admin.ex
@@ -0,0 +1,24 @@
+defmodule Commands.Admin do
+  alias Nostrum.Api
+
+  @admin_channel_id Application.fetch_env!(:trnp, :admin_channel_id)
+
+  def handle_command("set channel " <> command) do
+    [channel, id] = String.split(command)
+
+    case channel do
+      "selling" ->
+        id
+        |> String.to_integer()
+        |> Trnp.Selling.set_channel_id()
+
+      _ ->
+        nil
+    end
+  end
+
+  def handle_command(command) do
+    command = String.split(command)
+    Api.create_message!(@admin_channel_id, "Unknown command: #{command}")
+  end
+end
