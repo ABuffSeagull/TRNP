70caa15680291b9263aea7a135ea820c1cfe6930
ABuffSeagull <reecevanatta@gmail.com>
2020-04-18 19:03:54 -0400

    Add jason to the dependencies

--- mix.exs
+++ mix.exs
@@ -25,6 +25,7 @@ defmodule Trnp.MixProject do
       {:timex, "~> 3.6"},
       {:nostrum, "~> 0.4.1"},
       {:quantum, "~> 3.0.0-rc.3"},
+      {:jason, "~> 1.2"},
       {:credo, "~> 1.3", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
