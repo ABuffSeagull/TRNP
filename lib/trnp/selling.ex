0f8c746e0343c25e8d8af575856f45711bce390c
ABuffSeagull <reecevanatta@gmail.com>
2020-04-18 19:04:52 -0400

    Make a Selling struct
    That way we can have compile time guarantees of the fields,
    as well as better default creation

--- lib/trnp/selling.ex
+++ lib/trnp/selling.ex
@@ -9,26 +9,38 @@ defmodule Trnp.Selling do
   @channel_parent_id Application.fetch_env!(:trnp, :channel_parent_id)
   @admin_channel_id Application.fetch_env!(:trnp, :admin_channel_id)
 
+  ## Init
+
+  defstruct channel_id: nil, user_ids: %{}
+
   def start_link(_initial_state),
-    do: Agent.start_link(fn -> %{id: nil, prices: []} end, name: __MODULE__)
+    do: Agent.start_link(fn -> %__MODULE__{} end, name: __MODULE__)
+
+  ## Api
 
   def set_channel_id(id), do: Agent.cast(__MODULE__, &Map.replace!(&1, :id, id))
 
   def clean_channel, do: Agent.cast(__MODULE__, &remake_channel/1)
 
-  defp remake_channel(%{id: nil} = state) do
+  def get_history(id) do
+    Agent.get(__MODULE__, fn %__MODULE__{user_ids: user_ids} -> Map.get(user_ids, id, []) end)
+  end
+
+  ## Agent callbacks
+
+  defp remake_channel(%__MODULE__{channel_id: nil} = state) do
     Api.create_message!(@admin_channel_id, "You forgot to set the selling channel id, dumbass")
     state
   end
 
-  defp remake_channel(%{id: channel_id}) do
-    with {:ok, _channel} <- Api.delete_channel(channel_id, "Nightly wipe"),
-         {:ok, %{id: id}} <-
+  defp remake_channel(%__MODULE__{channel_id: channel_id}) do
+    with {:ok, _channel} <- Api.delete_channel(channel_id, "Weekly wipe"),
+         {:ok, %{channel_id: id}} <-
            Api.create_guild_channel(@guild_id,
              name: "selling-prices",
              parent_id: @channel_parent_id
            ) do
-      %{id: id, prices: []}
+      %__MODULE__{channel_id: id}
     else
       {:error, error} ->
         %{response: %{message: message}, status_code: code} = error
@@ -37,6 +49,8 @@ defmodule Trnp.Selling do
           @admin_channel_id,
           "Something went wrong with deleting the selling channel: #{code} #{message}"
         )
+
+        %__MODULE__{}
     end
   end
 end
