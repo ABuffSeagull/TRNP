d61ab0ce7b9d064f940513f84d63e0c7f4c99e26
ABuffSeagull <reecevanatta@gmail.com>
2020-04-19 01:11:35 -0400

    Add history command

--- lib/commands/user.ex
+++ lib/commands/user.ex
@@ -28,6 +28,39 @@ defmodule Commands.User do
     Api.create_message!(channel_id, "<@!#{user_id}> #{message}")
   end
 
+  def handle_command(%Message{
+        content: "!history" <> _extra,
+        author: %{id: user_id},
+        channel_id: channel_id
+      }) do
+    message =
+      user_id
+      |> Trnp.Selling.get_history()
+      |> Enum.to_list()
+      |> Enum.sort(fn {key1, _}, {key2, _} -> key1 <= key2 end)
+      |> Enum.map(&format_price/1)
+      |> Enum.join(", ")
+
+    Api.create_message!(channel_id, "<@!#{user_id}> #{message}")
+  end
+
   # Fallthrough
   def handle_command(_message), do: nil
+
+  defp format_price({key, price}) do
+    [day, time] = Regex.run(~r/(?<day>\d)_(?<time>am|pm)/, key, capture: :all_but_first)
+
+    day =
+      case day do
+        "0" -> "Sunday"
+        "1" -> "Monday"
+        "2" -> "Tuesday"
+        "3" -> "Wednesday"
+        "4" -> "Thursday"
+        "5" -> "Friday"
+        "6" -> "Saturday"
+      end
+
+    "#{day} #{String.upcase(time)}: #{price}"
+  end
 end
