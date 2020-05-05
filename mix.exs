76b5a07a10ed3d42191902ca43ef658aea7595ba
ABuffSeagull <reecevanatta@gmail.com>
2020-05-04 22:12:58 -0400

    v3.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Trnp.MixProject do
   def project do
     [
       app: :trnp,
-      version: "3.0.2",
+      version: "3.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
